4cb6c23839e58888dc7f9f6986eab81c8c299867
Annie Wu <wu.annie.j@gmail.com>
2016-06-01 21:55:41 -0500

    Added example code from LED cube for reference. Updated fingers.c

--- fingers.c
+++ fingers.c
@@ -1,2 +1,19 @@
-// fingers.c
+/** fingers.c
+*
+*
+*/
 
+#include <fingers.h>
+#include <stdio.h>
+
+
+
+struct finger()
+{
+    // Array ActivatedDirections [Up, Down, Left, Right]
+    // Will have a value of true or false for each one. Always size 4.
+    // Default values is false for all, aka nothing is activated.
+    boolean ActivatedDirections [4] = [false, false, false, false];
+    
+    
+}
