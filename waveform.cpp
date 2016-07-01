d7ab3c52e61b717830c8ca8d69cf6e8e93c0b481
juliajohn0207 <jxj121130@utdallas.edu>
2016-07-01 08:54:19 -0500

    waveform.cpp
    not official

---
+++ waveform.cpp
@@ -0,0 +1,44 @@
+#include "Arduino.h"
+#include "waveform.h"
+
+
+
+
+/*
+
+Waveform :: Waveform() 
+{
+
+	//insert function
+}
+
+
+Waveform :: Waveform(int wf_, float vtg_, float curr_)
+{
+
+	waveform = wf_;
+	voltage = vtg_;
+	current = Curr_ ;
+}
+
+
+void Waveform :: toggle ()
+{
+
+	//insert function
+}
+
+
+void Waveform :: activate(int wf_, float vtg_, float _current)
+{
+
+	//insert function
+}
+
+void Waveform :: deactivate(int wf_, float vtg_, float _current)
+{
+	//insert function
+}
+
+
+*/
